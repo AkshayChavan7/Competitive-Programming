d4acecc63000a530a87c6166413ac6d20996b162
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2022-01-03 21:15:19 +0530

    p3 - longest substring without repeating characters

---
+++ Leetcode/p3 - longest substring without repeating characters.cpp
@@ -0,0 +1,50 @@
+// Leetcode
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+class Solution {
+public:
+    int mLen = 0;
+    
+    // recursion function
+    void maxSubStringLength(string s, int prevMaxLen) {
+        
+        int maxLen = 0;
+        string substr = "";
+        int recursionFlag = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(substr.find(s[i])<=substr.length()) {        // substr contains char
+                recursionFlag = 0;
+                for(int j = i-1; j>=0; j--) {
+                    if(s[j] == s[i]) {
+                        // cout<<"maxSubStringLength("<<s.substr(j+1)<<","<<substr.length()<<")"<<endl;
+                        maxSubStringLength(s.substr(j+1),substr.length());     
+                        recursionFlag = 1;
+                        break;
+                    }
+                }
+                if(recursionFlag) {
+                    break;
+                }
+            } else {
+                substr +=s[i];
+            }
+        }
+        
+        if(substr.length()>prevMaxLen) {
+            if(substr.length()>mLen) {
+                mLen = substr.length();
+            }
+        } 
+        // else {
+        //     // return prevMaxLen;
+        //     // mLen = prevMaxLen;
+        // }
+    }
+    
+    int lengthOfLongestSubstring(string s) {
+        
+        maxSubStringLength(s,0);
+        return mLen;
+    }
+};
