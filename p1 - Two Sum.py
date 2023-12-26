ff37fa3e5832ec134d4121fb9081222080a1bb52
Akshay Chavan <chavan.ak@northeastern.edu>
2023-12-26 03:16:00 -0500

    [1] Two Sum

---
+++ p1 - Two Sum.py
@@ -0,0 +1,7 @@
+# Two Sum - https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if (target-nums[i]) in nums[i+1:]:
+                return [nums.index(nums[i]), nums[i+1:].index(target-nums[i])+i+1]
