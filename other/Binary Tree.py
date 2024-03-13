95e69da67696c3f57b6317dd9c671ed93743cd4b
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-03-13 18:42:45 -0400

    Binary Tree DFS

--- other/Binary Tree.py
+++ other/Binary Tree.py
@@ -27,7 +27,7 @@ c.right = f
          d    e       f
 '''
 
-# Depth First Traversal
+# Depth First Traversal - Iterative
 def DFS(root:Node):
     if root == None: return
     stack = [root]
@@ -38,7 +38,33 @@ def DFS(root:Node):
         if current.right: stack.append(current.right)
         if current.left: stack.append(current.left)
 
-DFS(None)
+print('Iterative DFS output =>')
+DFS(a)
 
 # Time: O(n)
 # Space: O(n)
+
+# Depth First Traversal - Recursive
+def DFS_recursive(root:Node):
+    if not root: return
+    print(root.val)
+    DFS_recursive(root.left)
+    DFS_recursive(root.right)
+
+print('Recursive DFS output =>')
+DFS_recursive(a)
+
+# Time: O(n)
+# Space: O(n)
+
+# Depth First Traversal - Recursive 
+# Instead of printing the values, we will store them in a list
+# and return that as a result
+def DFS_recursive2(root:Node):
+    if not root: return []
+    leftValues = DFS_recursive2(root.left) # [b, d, e]
+    rightValues = DFS_recursive2(root.right) # [c, f]
+    return [root.val] + leftValues + rightValues
+
+print('Recursive DFS output as list =>')
+print(DFS_recursive2(a))
