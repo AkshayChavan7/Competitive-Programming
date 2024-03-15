b3f3a0f9ab0dd9bf131a6ecb6ac1701423aff603
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-03-14 20:40:26 -0400

    199. Binary Tree Right Side View

--- 199. Binary Tree Right Side View.py
+++ 199. Binary Tree Right Side View.py
@@ -6,23 +6,39 @@
 #         self.right = right
 from collections import deque
 class Solution:
+    # Single Pass Solution
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root: return []
         queue = deque()
         queue.append((root, 0))
-        tree = []
+        previous = (root, -1)
+        result = []
         while len(queue)>0:
             current, level = queue.popleft()
-            tree.append((current, level))
-            if current.left: queue.append((current.left, level+1))
+            if previous[1] != level: 
+                result.append(current.val)
+            previous = (current, level)
             if current.right: queue.append((current.right, level+1))
-        
-        result = []
-        for i in range(len(tree)-1):
-            if tree[i][1]!=tree[i+1][1]:
-                result.append(tree[i][0].val)
-        result.append(tree[-1][0].val)
+            if current.left: queue.append((current.left, level+1))
         return result
-
+    
+    # Two Pass Solution
+    # def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    #     if not root: return []
+    #     queue = deque()
+    #     queue.append((root, 0))
+    #     tree = []
+    #     while len(queue)>0:
+    #         current, level = queue.popleft()
+    #         tree.append((current, level))
+    #         if current.left: queue.append((current.left, level+1))
+    #         if current.right: queue.append((current.right, level+1))
+        
+    #     result = []
+    #     for i in range(len(tree)-1):
+    #         if tree[i][1]!=tree[i+1][1]:
+    #             result.append(tree[i][0].val)
+    #     result.append(tree[-1][0].val)
+    #     return result
 
 
